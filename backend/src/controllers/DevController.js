cc08e3204af716ac94db53885de99c0ded9b7180
Daniel <Daniel.Versiane@gmail.com>
2020-01-15 01:16:03 -0300

    Função Update e Delete.

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -35,5 +35,28 @@ module.exports = {
         }
 
         return response.json(dev);
-    }
+    },
+
+    async update(request, response) {
+        const { github_username } = request.params;
+        const { bio } = request.body;
+
+        var newvalues = { $set: { bio: bio } };
+        let dev = await Dev.updateOne({github_username:github_username}, newvalues, function (err, res) {
+            console.log(res)
+        })
+
+
+        return response.json({ dev });
+    },
+
+    async destroy(request, response) {
+        const { github_username } = request.params;
+        let dev = await Dev.findOne({ github_username });
+        dev = await Dev.deleteOne({
+            github_username: github_username
+        });
+
+        return response.json({ dev });
+    },
 }
